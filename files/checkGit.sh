4a9940c50828acfb0bdfd87d46ca71e9b53abb3d
se-cloud-demo-service <83480893+se-cloud-demo-service@users.noreply.github.com>
2021-05-04 15:01:05 +0200

    change to service account

--- files/checkGit.sh
+++ files/checkGit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 REPOOUTPUT=$(curl -k -u $TL_USER:$TL_PASS \
   -H 'Content-Type: application/json' \
-  https://$TL_CONSOLE/api/v1/coderepos?id=se-cloud-emea-demo%2F<buildname>-evil.petclinic&limit=15&offset=0&project=Central+Console&reverse=true&sort=vulnerabilityRiskScore)
+  https://$TL_CONSOLE/api/v1/coderepos?id=se-cloud-demo-service%2F<buildname>-evil.petclinic&limit=15&offset=0&project=Central+Console&reverse=true&sort=vulnerabilityRiskScore)
 #in row nr 4. replace <buildname> with your buildname of your demo-build.yml file. 
 
 #sed -n -e 's/^.*\(vulnerabilitiesCount\)/\1/p' | cut -f1 -d, | cut -f2- -d: > output.txt
