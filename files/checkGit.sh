eea2a5a455d6894f2f67ae42b59c83571aa6ce84
GBaileyMcEwan <39169795+GBaileyMcEwan@users.noreply.github.com>
2021-05-17 10:12:30 +0200

    Update checkGit.sh
    Updated comment

--- files/checkGit.sh
+++ files/checkGit.sh
@@ -2,10 +2,11 @@
 #Force a rescan of the repo in order to get fresh results
 curl -k -u $TL_USER:$TL_PASS -H 'Content-Type: application/json' -X POST https://$TL_CONSOLE/api/v1/coderepos/scan
 sleep 5
+#replace <buildname> with your buildname of your demo-build.yml file.
 REPOOUTPUT=$(curl -k -u $TL_USER:$TL_PASS \
   -H 'Content-Type: application/json' \
   https://$TL_CONSOLE/api/v1/coderepos?id=se-cloud-demo-service%2F<buildname>-evil.petclinic&limit=15&offset=0&project=Central+Console&reverse=true&sort=vulnerabilityRiskScore)
-#in row nr 4. replace <buildname> with your buildname of your demo-build.yml file. 
+ 
 
 #sed -n -e 's/^.*\(vulnerabilitiesCount\)/\1/p' | cut -f1 -d, | cut -f2- -d: > output.txt
 VULN=$(echo $REPOOUTPUT | sed -n -e 's/^.*\(vulnerabilitiesCount\)/\1/p' | cut -f1 -d, | cut -f2- -d:)
