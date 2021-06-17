c886def7965c1ab3a384701892c61b56b2fb480e
mkhammash <muhannad.khammash@hotmail.com>
2021-06-17 17:07:53 +0400

    Update checkGit.sh

--- files/checkGit.sh
+++ files/checkGit.sh
@@ -5,7 +5,7 @@ sleep 5
 #replace <buildname> with your buildname of your demo-build.yml file.
 REPOOUTPUT=$(curl -k -u $TL_USER:$TL_PASS \
   -H 'Content-Type: application/json' \
-  https://$TL_CONSOLE/api/v1/coderepos?id=se-cloud-demo-service%2F<buildname>-evil.petclinic&limit=15&offset=0&project=Central+Console&reverse=true&sort=vulnerabilityRiskScore)
+  https://$TL_CONSOLE/api/v1/coderepos?id=mkhammash%2Fbase-evil.petclinic&limit=15&offset=0&project=Central+Console&reverse=true&sort=vulnerabilityRiskScore)
  
 
 #sed -n -e 's/^.*\(vulnerabilitiesCount\)/\1/p' | cut -f1 -d, | cut -f2- -d: > output.txt
